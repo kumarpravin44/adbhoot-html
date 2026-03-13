41c94bea3d217917f9dfe3b55410328558902437
Pravin Kumar <kumar.pravin160@gmail.com>
2026-03-13 12:37:02 +0530

    Your commit message

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+hamburger.addEventListener("click", () => {
+
+hamburger.classList.toggle("active");
+menu.classList.toggle("active");
+
+});
+
+
+$(window).scroll(function(){
+  var sticky = $('.creative-nav'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= 900) sticky.addClass('fixed');
+  else sticky.removeClass('fixed');
+});
